4542fd476915b4a6ed3025075935d4a6a7c2f293
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-10-01 22:41:58 +0100

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskList from "./TaskList";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
-
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { TASKS, CATEGORIES } from "../data";
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [filteredCategory, setFilteredCategory] = useState("All");
+
+  const handleCategoryChange = (category) => {
+    setFilteredCategory(category);
+  };
+
+  const handleTaskFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleTaskDelete = (task) => {
+    setTasks(tasks.filter((t) => t !== task));
+  };
+
+  const filteredTasks = filteredCategory === "All"
+    ? tasks
+    : tasks.filter((task) => task.category === filteredCategory);
+
   return (
     <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={filteredCategory}
+        onCategoryChange={handleCategoryChange}
+      />
+      <TaskList tasks={filteredTasks} onDelete={handleTaskDelete} />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleTaskFormSubmit} />
     </div>
   );
 }
-
 export default App;
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [taskText, setTaskText] = useState("");
+  const [taskCategory, setTaskCategory] = useState(categories[0]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTask = {
+      text: taskText,
+      category: taskCategory,
+    };
+
+    onTaskFormSubmit(newTask);
+
+    setTaskText("");
+    setTaskCategory(categories[0]);
+  };
+
+  const handleTaskTextChange = (e) => {
+    setTaskText(e.target.value);
+  };
+
+  const handleTaskCategoryChange = (e) => {
+    setTaskCategory(e.target.value);
+  };
+
   return (
-    <form className="new-task-form">
-      <label>
-        Details
-        <input type="text" name="text" />
-      </label>
-      <label>
-        Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
-        </select>
-      </label>
-      <input type="submit" value="Add task" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Task description"
+        value={taskText}
+        onChange={handleTaskTextChange}
+      />
+      <select
+        value={taskCategory}
+        onChange={handleTaskCategoryChange}
+      >
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+      <button type="submit">Add Task</button>
     </form>
   );
 }
